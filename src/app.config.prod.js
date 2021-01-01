c31892eb19d59aeb4a403f98e116ebeef5019dda
Tống Minh Đức <shanee1998@gmail.com>
2021-01-01 09:04:45 +0700

    chore: re run actions

--- src/app.config.prod.js
+++ src/app.config.prod.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://40.75.0.139:4000';
-export const APP_URL = 'http://40.75.0.139';
+export const API_URL = 'http://104.209.148.218:4000';
+export const APP_URL = 'http://104.209.148.218';

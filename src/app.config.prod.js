a6dd4fc97a3ebbb02a0d7caafd7cff8f4023495e
Tống Minh Đức <shanee1998@gmail.com>
2021-01-14 00:19:02 +0700

    improv: update url api

--- src/app.config.prod.js
+++ src/app.config.prod.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://157.230.243.182:4000';
-export const APP_URL = 'http://157.230.243.182';
+export const API_URL = 'http://52.167.219.69:4000';
+export const APP_URL = 'http://52.167.219.69';
